af836733a512257021853824095e558768a9913e
IvanVZabrodin <ivanvzabrodin@outlook.com>
2024-03-16 15:18:01 +0000

    Renamed to PID Controller in order to prepare for L1

--- plotter.py
+++ plotter.py
@@ -16,7 +16,7 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-import simulation as sim
+import pidsimulation as sim
 import matplotlib.pyplot as plt
 from emath import *
 
